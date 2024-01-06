cb5e8e1db03ca7ad59d6a595b0b8f3a57ac23add
deividas48 <deividas.v6@gmail.com>
2024-01-06 22:50:16 +0200

    Create node server and add env file

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// Import modules for the server setup.
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+
+const app = express(); // Initialize an Express application
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+//Send 'Hello World!' when the page is visited.
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+// Start the server and listen on the PORT
+app.listen(PORT, () => { console.log(`Server listening at http://localhost:${PORT}`); 
+});

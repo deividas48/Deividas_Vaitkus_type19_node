31a64558d1938890e671e11ad2e20edef35170df
deividas48 <deividas.v6@gmail.com>
2024-01-07 00:55:28 +0200

    Install ESLint and run

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-
 const app = express(); // Initialize an Express application
 const PORT = process.env.PORT || 5000;
 
@@ -12,11 +11,12 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
 
-//Send 'Hello World!' when the page is visited.
+// Send 'Hello World!' when the page is visited.
 app.get('/', (req, res) => {
-res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 // Start the server and listen on the PORT
-app.listen(PORT, () => { console.log(`Server listening at http://localhost:${PORT}`); 
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });

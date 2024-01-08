8ff800ddeab67023e3960a759a32c9cc3e450791
deividas48 <deividas.v6@gmail.com>
2024-01-08 22:09:04 +0200

    Style check and other small fixes

--- src/config.js
+++ src/config.js
@@ -6,13 +6,13 @@ const mysql = require('mysql2/promise');
 
 // Use .env values to set up the database connection.
 const dbConfig = mysql.createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-  });
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+});
 
-  // Export the configuration.
-  module.exports = {
-    dbConfig,
-    };
+// Export the configuration.
+module.exports = {
+  dbConfig,
+};
--- src/helper.js
+++ src/helper.js
@@ -5,23 +5,23 @@ const { dbConfig } = require('./config');
 
 // Create a helper function to execute the query
 async function dbQueryWithData(sql, argArr = []) {
-    let conn;
-    try {
-      // Connect to the database
-      conn = await mysql.createConnection(dbConfig);
-      // Execute the query
-      const [rows] = await conn.execute(sql, argArr);
-      // Return the results
-      return [rows, null];
-    } catch (error) {
-      return [null, error];
-    } finally {
-      // Close the connection
-      if (conn) conn.end();
-    }
+  let conn;
+  try {
+    // Connect to the database
+    conn = await mysql.createConnection(dbConfig);
+    // Execute the query
+    const [rows] = await conn.execute(sql, argArr);
+    // Return the results
+    return [rows, null];
+  } catch (error) {
+    return [null, error];
+  } finally {
+    // Close the connection
+    if (conn) conn.end();
   }
-  
-    // Export the helper function  
-  module.exports = {
-    dbQueryWithData,
-  };
+}
+
+// Export the helper function
+module.exports = {
+  dbQueryWithData,
+};

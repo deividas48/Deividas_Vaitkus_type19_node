9a5a2fe024b6e862e642cc53a5a6cba6875858fd
deividas48 <deividas.v6@gmail.com>
2024-01-08 19:07:26 +0200

    Create config.js file and DB connection

---
+++ src/config.js
@@ -0,0 +1,18 @@
+// Import the dotenv module to read the .env file.
+require('dotenv').config();
+
+// Import the mysql module.
+const mysql = require('mysql2/promise');
+
+// Use .env values to set up the database connection.
+const dbConfig = mysql.createPool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+  });
+
+  // Export the configuration.
+  module.exports = {
+    dbConfig,
+    };

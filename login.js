0909d4948aa057bde738563b2ed5f6a6a499c043
deividas48 <deividas.v6@gmail.com>
2024-01-10 06:38:50 +0200

    Create login

---
+++ login.js
@@ -0,0 +1,24 @@
+document
+  .getElementById('registration-form')
+  .addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    fetch('/api/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  });
